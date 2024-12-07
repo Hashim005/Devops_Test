ec9b679617092475708804ef952358311908a535
Hashim005 <117928947+Hashim005@users.noreply.github.com>
2024-12-07 16:51:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/server', (req, res) => {
-    res.send("Hello DevOps hashim ");
+    res.send("Hello DevOps ");
 })
 
 app.use(express.static('public'))
